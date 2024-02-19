59ed45325a94172abc7376929d6e4d9abb35f48a
Ankit Prajapati <ankitprajapati.user@gmail.com>
2024-02-19 12:14:25 +0530

    getting entity and repo from core module

---
+++ core/src/test/java/com/cms/core/employeemanagement/entity/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.core.employeemanagement.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
